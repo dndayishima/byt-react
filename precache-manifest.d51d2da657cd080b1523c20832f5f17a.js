73b7d2e153ead05234973f938b6214e582335149
dndayishima <divin.ndayishima@outlook.com>
2019-08-05 16:49:31 +0200

    Updates

---
+++ precache-manifest.d51d2da657cd080b1523c20832f5f17a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800b0d6794efb1035e463972986a29ca",
+    "url": "/byt-react/index.html"
+  },
+  {
+    "revision": "23b2a6168d56d0c8cc1c",
+    "url": "/byt-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2f04602724064d1f5e2f",
+    "url": "/byt-react/static/js/2.1f70ba9c.chunk.js"
+  },
+  {
+    "revision": "23b2a6168d56d0c8cc1c",
+    "url": "/byt-react/static/js/main.87212732.chunk.js"
+  },
+  {
+    "revision": "b12faef54d7dd7f28147",
+    "url": "/byt-react/static/js/runtime~main.8c6eab6b.js"
+  },
+  {
+    "revision": "83e9ffb76e6d9b0a9411f5b5d802309a",
+    "url": "/byt-react/static/media/byt-logo.83e9ffb7.jpg"
+  },
+  {
+    "revision": "838435956ca64516b762f312af867548",
+    "url": "/byt-react/static/media/byt-white.83843595.png"
+  }
+]);
