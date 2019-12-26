03889ef81bf13c1706190cc0029c0f479abfa1df
dndayishima <divin.ndayishima@outlook.com>
2019-12-26 13:25:27 +0100

    Updates

---
+++ precache-manifest.ddf21467b6d5b36240ba9076889f50d6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a166a006f35a4f27594d14da64ecca",
+    "url": "/byt-react/index.html"
+  },
+  {
+    "revision": "e4419e0359faf881a11e",
+    "url": "/byt-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a385d7efb28781d51f83",
+    "url": "/byt-react/static/js/2.08375116.chunk.js"
+  },
+  {
+    "revision": "e4419e0359faf881a11e",
+    "url": "/byt-react/static/js/main.c812f136.chunk.js"
+  },
+  {
+    "revision": "3d0229b3aaa2fde556c9",
+    "url": "/byt-react/static/js/runtime-main.3dc89232.js"
+  },
+  {
+    "revision": "62895e2421de9643a1122dcaff8199ac",
+    "url": "/byt-react/static/media/bancobu.62895e24.jpg"
+  },
+  {
+    "revision": "83e9ffb76e6d9b0a9411f5b5d802309a",
+    "url": "/byt-react/static/media/byt-logo.83e9ffb7.jpg"
+  },
+  {
+    "revision": "838435956ca64516b762f312af867548",
+    "url": "/byt-react/static/media/byt-white.83843595.png"
+  },
+  {
+    "revision": "0e56d2784c5eb9eb0b63e32ea3a37636",
+    "url": "/byt-react/static/media/econet.0e56d278.png"
+  },
+  {
+    "revision": "c023213b08f37732cf179c593cc6591e",
+    "url": "/byt-react/static/media/favicon_byt.c023213b.jpg"
+  },
+  {
+    "revision": "49caf25f43263655522125f25133b53c",
+    "url": "/byt-react/static/media/finbank.49caf25f.jpg"
+  },
+  {
+    "revision": "8f7a8550f6bf6a6bc2a989b41b532ea6",
+    "url": "/byt-react/static/media/image-empty.8f7a8550.png"
+  },
+  {
+    "revision": "d55ec26032efa812ffe9d2817d16c183",
+    "url": "/byt-react/static/media/lumitel.d55ec260.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/byt-react/precache-manifest.6b7857484a6974ae4a5d03856bd0eb0a.js"
+  "/byt-react/precache-manifest.ddf21467b6d5b36240ba9076889f50d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/byt-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
