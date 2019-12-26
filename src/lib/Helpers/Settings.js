cbc592c0a2d994b872eb384608ce6bd132347d32
dndayishima <divin.ndayishima@outlook.com>
2019-12-26 15:57:31 +0100

    Change url domains

--- src/lib/Helpers/Settings.js
+++ src/lib/Helpers/Settings.js
@@ -7,8 +7,8 @@
 //const apiUrl = "http://localhost:8081";
 
 // de mode production
-const authUrl = "https://demo2.auth.bytpayment.com";
-const apiUrl = "https://demo2.services.bytpayment.com";
+const authUrl = "https://demoauth2.bytpayment.com";
+const apiUrl = "https://demoservices2.bytpayment.com";
 // devises disponibles
 const currencies = [
   { currency: "BIF", symbol: "BIF" },
